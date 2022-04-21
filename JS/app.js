8cc91f77c563d0614e8eb9f9e135a97716bafbde
Juan Hernandez <juanhb9@gmail.com>
2022-04-20 21:50:07 -0500

    finalizando menu js

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,12 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
-navToggle.addEventListener("click", ()=>{
+navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
   //pequeña modificación para ser nuestro menú accesible
-    if (navMenu.classList.contains("nav-menu_visible")){
+    if (navMenu.classList.contains("nav-menu_visible")) {
         navToggle.setAttribute("aria-label", "Cerrar menú");
-    }else{
+    } else {
         navToggle.setAttribute("aria-label", "Abrir menú");
     }
 // fin de pequeña modificación
